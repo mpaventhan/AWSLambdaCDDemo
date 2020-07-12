b3649d18d200efdbf0f4b14dadadcbad7306d459
mpaventhan <66654636+mpaventhan@users.noreply.github.com>
2020-07-11 19:08:53 -0700

    Removed a line

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -6,5 +6,4 @@ ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
-    print("test")
     return {"statusCode": 200, "body": json.dumps(response)}
